4e0af3b886a3bbe0e3513481314392e0db7b43d7
scxrchy <71168355+scottarsenjr@users.noreply.github.com>
2023-02-01 20:28:37 +0300

    Initial commit

--- code/editor.py
+++ code/editor.py
@@ -29,11 +29,36 @@ class Editor:
         if not mouse_btns()[1]:
             self.pan_active = False
 
+        if event.type == pygame.MOUSEWHEEL:
+            if pygame.key.get_pressed()[pygame.K_LCTRL]:
+                self.origin.y -= event.y * 50
+            else:
+                self.origin.x -= event.y * 50
+
         # panning update
         if self.pan_active:
             self.origin = vector(mouse_pos()) - self.pan_offset
 
+    def draw_tile(self):
+        cols = WINDOW_WIDTH // TILE_SIZE
+        rows = WINDOW_HEIGHT // TILE_SIZE
+
+        offset_vector = vector(
+            x=self.origin.x - int(self.origin.x / TILE_SIZE) * TILE_SIZE,
+            y=self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE
+        )
+
+        for col in range(cols + 1):
+            x = offset_vector.x + col * TILE_SIZE
+            pygame.draw.line(self.display_surface, LINE_COLOR, (x, 0), (x, WINDOW_HEIGHT))
+
+        for row in range(rows + 1):
+            y = offset_vector.y + row * TILE_SIZE
+            pygame.draw.line(self.display_surface, LINE_COLOR, (0, y), (WINDOW_WIDTH, y))
+
     def run(self, tickrate):
-        self.display_surface.fill('white')
         self.event_loop()
+        self.display_surface.fill('white')
+
+        self.draw_tile()
         pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
